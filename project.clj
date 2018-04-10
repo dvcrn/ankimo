34c3c4e4a6482d5d3e66d806489dc0b0e5bc637c
David Mohl <git@davemail.io>
2018-04-11 07:34:35 +0900

    chore: release 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ankimo "0.1.0"
+(defproject ankimo "0.1.1"
   :description "Browser extension for sending Japanese words to Anki"
   :url "https://github.com/dvcrn/ankimo"
   :license {:name "MIT"
